7f4b801b9218ec53eabf6dad4143b570332b8d64
Lee Hart <4256466+leehart@users.noreply.github.com>
2018-06-07 10:03:41 +0100

    Increase white-space to 2px, rejig

--- config/analytics/components/SiteMarker.js
+++ config/analytics/components/SiteMarker.js
@@ -22,48 +22,48 @@ let SiteMarker = createReactClass({
     } = this.props;
     const colourFunc = propertyColour(this.config.tablesById['sites'].propertiesById['ARTresistance']);
     return (
-      <svg style={{width: "30px", height:"24px", transform: "translate(-15px,-24px)"}}>
+      <svg style={{width: "34px", height:"27px", transform: "translate(-17px,-27px)"}}>
         <g>
-          <rect x="0" y="0" width="30" height="21" rx="5" ry="5" style={{fill: 'black'}}/>
-          <rect x="1" y="1" width="28" height="19" rx="3" ry="3" style={{fill: 'white'}}/>
+          <rect x="0" y="0" width="34" height="24" rx="5" ry="5" style={{fill: 'black'}}/>
+          <rect x="1" y="1" width="32" height="22" rx="4" ry="4" style={{fill: 'white'}}/>
           <circle
             style={{fill:colourFunc(ARTresistance)}}
-            cx="6"
-            cy="6"
+            cx="7"
+            cy="7"
             r="4"
           />
           <circle
             style={{fill:colourFunc(CQresistance)}}
-            cx="15"
-            cy="6"
+            cx="17"
+            cy="7"
             r="4"
           />
           <circle
             style={{fill:colourFunc(MQresistance)}}
-            cx="24"
-            cy="6"
+            cx="27"
+            cy="7"
             r="4"
           />
           <circle
             style={{fill:colourFunc(PPQresistance)}}
-            cx="6"
-            cy="15"
+            cx="7"
+            cy="17"
             r="4"
           />
           <circle
             style={{fill:colourFunc(PYRresistance)}}
-            cx="15"
-            cy="15"
+            cx="17"
+            cy="17"
             r="4"
           />
           <circle
             style={{fill:colourFunc(SDXresistance)}}
-            cx="24"
-            cy="15"
+            cx="27"
+            cy="17"
             r="4"
           />
-          <polygon points="12,21 15,24 18,21" style={{fill: 'black'}}/>
-          <polygon points="12,20 15,23 18,20" style={{fill: 'white'}}/>
+          <polygon points="14,24 17,27 20,24" style={{fill: 'black'}}/>
+          <polygon points="14,23 17,26 20,23" style={{fill: 'white'}}/>
         </g>
       </svg>
     );
