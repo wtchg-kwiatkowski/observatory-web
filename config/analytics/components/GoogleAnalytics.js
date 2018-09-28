549e033a704e3f18d5d0c0d8f00e4f20b935cfed
Lee Hart <4256466+leehart@users.noreply.github.com>
2018-09-28 17:59:46 +0100

    WIP: develop GoogleAnalytics custom component

--- config/analytics/components/GoogleAnalytics.js
+++ config/analytics/components/GoogleAnalytics.js
@@ -2,11 +2,18 @@ import createReactClass from 'create-react-class';
 import ReactGA from 'react-ga';
 import FluxMixin from 'mixins/FluxMixin';
 
-ReactGA.initialize('UA-foobar219381209381', {
+// FIXME: Use the appropriate Tracking ID for each environment.
+// Development: UA-126631386-2
+// Staging: UA-126631386-3
+// Production: UA-126631386-1
+
+ReactGA.initialize('UA-126631386-2', {
   debug: true,
+  // Why is the default titleCase true? https://github.com/react-ga/react-ga/issues/231
   titleCase: false, //Boolean. Optional. Defaults to true. If set to false, strings will not be converted to title case before sending to GA.
 });
-ReactGA.pageview('/about/contact-us');
+// Improve Google Analytics GDPR compliancy https://www.cookiebot.com/en/google-analytics-gdpr/
+ReactGA.set({anonymizeIp: true}); // https://developers.google.com/analytics/devguides/collection/analyticsjs/ip-anonymization
 
 // Chrome extension GA debugger https://chrome.google.com/webstore/detail/google-analytics-debugger/jnkmfdileelhofjcijamephohjechhna
 
@@ -19,11 +26,18 @@ let GoogleAnalytics = createReactClass({
 
   componentDidMount() {
     let store = this.getFlux().store('SessionStore');
-    store.on('change', () => console.log('change store', store));
+    store.on('change', () => {
+      const state = store.getState();
+      const selectedTabComponentKey = state.get('tabs').get('selectedTab');
+      const selectedTabComponentType = state.get('components').get(selectedTabComponentKey).get('type');
+      const selectedTabComponentProps = state.get('components').get(selectedTabComponentKey).get('props').toObject();
+      const selectedTabComponentPropsAsQueryString = Object.entries(selectedTabComponentProps).map(([key, value]) => `${encodeURIComponent(key)}=${encodeURIComponent(value)}`).join('&');
+      const pageViewPath = `${selectedTabComponentType}?${selectedTabComponentPropsAsQueryString}`;
+      ReactGA.pageview(pageViewPath);
+    });
   },
 
   render() {
-console.log('GA render')
     return null;
   }
 });
