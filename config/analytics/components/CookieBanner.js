527321e63a36940f388b585031c378b4a64f36d6
Lee Hart <4256466+leehart@users.noreply.github.com>
2018-10-03 16:40:44 +0100

    WIP: develop CookieBanner

--- config/analytics/components/CookieBanner.js
+++ config/analytics/components/CookieBanner.js
@@ -1,22 +1,54 @@
 import createReactClass from 'create-react-class';
 import React from 'react';
-import ReactCookieBanner from 'react-cookie-banner';
+import CookieConsent, {Cookies} from 'react-cookie-consent';
+import FluxMixin from 'mixins/FluxMixin';
 
 let CookieBanner = createReactClass({
   displayName: 'CookieBanner',
 
+  mixins: [
+    FluxMixin,
+  ],
+
+  componentWillMount() {
+    this.consentCookieName = 'userGaveCookieConsent';
+
+    // Credit https://dev.to/corbindavenport/how-to-correctly-check-for-do-not-track-with-javascript-135d
+    this.doNotTrack = (window.doNotTrack == '1' || navigator.doNotTrack == 'yes' || navigator.doNotTrack == '1' || navigator.msDoNotTrack == '1') ? true : false;
+    if (this.doNotTrack) {
+      // If doNotTrack then remove the consent-cookie and do not track.
+      Cookies.remove(this.consentCookieName);
+      console.info('Do not track.');
+    }
+    console.info('Cookies:', Object.keys(Cookies.get()));
+  },
+
+  handleAccept() {
+    console.log('TODO: trigger GoogleAnalytics');
+  },
+
   render() {
-    return (
-      <div>
-        <ReactCookieBanner
-          message="This website uses cookies."
-          buttonMessage="OK"
-          cookie="user-has-accepted-cookies"
-          link={<a href="https://www.malariagen.net/cookie-statement" target="_blank" rel="noreferrer noopener">cookie statement</a>}
-        />
-      </div>
-    );
+    if (this.doNotTrack) {
+      return null;
+    } else {
+      // CookieConsent settings
+      // `expires` should be a number, i.e. "Number of days before the cookie expires.", not a string. The default is 365.
+      // `debug` set to true prevents the banner from disappearing
+      return (
+        <CookieConsent
+          cookieName={this.consentCookieName}
+          expires={365}
+          buttonText="Consent"
+          buttonStyle={{backgroundColor: '#F7F7F7', borderRadius: '3px'}}
+          style={{textAlign: 'center'}}
+          onAccept={this.handleAccept}
+        >
+          We would like to use cookies to help improve this website&#8217;s performance and your experience. See our <a href="https://www.malariagen.net/cookie-statement" target="_blank" rel="noreferrer noopener">cookie statement</a> for details.
+        </CookieConsent>
+      );
+    }
   }
+
 });
 
 export default CookieBanner;
