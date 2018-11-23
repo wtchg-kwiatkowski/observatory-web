c398eb331e2b0e6767142b0a6859cfa9cc8e4e5b
Lee Hart <4256466+leehart@users.noreply.github.com>
2018-11-23 13:58:01 +0000

    Add footer to resistance map

--- config/analytics/components/ResistanceMap.js
+++ config/analytics/components/ResistanceMap.js
@@ -50,117 +50,123 @@ let ResistanceMap = createReactClass({
     // getDefaultProps doesn't handle empty string
     drug = drug === undefined || drug === '' ? 'sites' : drug;
     return (
-      <div className="obs-page-container centering-container">
-        <Card>
-          <CardContent>
-            <Typography variant="headline">
-                Global resistance status
-            </Typography>
-            {drug !== 'sites' ?
-              <Typography variant="subheading" color="textSecondary">
-                Each marker represents a site with parasite genome data. The area of the circle is proportional to the total number of samples collected at that site. Click on a different drug or site for further details.
+      <div className="obs-page-container">
+        <div className="centering-container">
+          <Card>
+            <CardContent>
+              <Typography variant="headline">
+                  Global resistance status
               </Typography>
-              :
-              <Typography variant="subheading" color="textSecondary">
-                Each marker represents a site with parasite genome data. This summary shows the proportion of resistant parasites at six  antimalarial drugs. Click a drug or site for further details.
-              </Typography>
-            }
+              {drug !== 'sites' ?
+                <Typography variant="subheading" color="textSecondary">
+                  Each marker represents a site with parasite genome data. The area of the circle is proportional to the total number of samples collected at that site. Click on a different drug or site for further details.
+                </Typography>
+                :
+                <Typography variant="subheading" color="textSecondary">
+                  Each marker represents a site with parasite genome data. This summary shows the proportion of resistant parasites at six  antimalarial drugs. Click a drug or site for further details.
+                </Typography>
+              }
 
-            <FormControl component="fieldset">
-              <RadioGroup
-                aria-label="drug"
-                name="drug"
-                // className={classes.group}
-                value={drug}
-                onChange={this.handleChange}
-                style={{flexDirection: 'row'}}
-              >
-                <FormControlLabel value="sites" control={<Radio/>} label="All drugs"/>
-                <FormControlLabel value="ART" control={<Radio/>} label="Artemisinin"/>
-                <FormControlLabel value="CQ" control={<Radio/>} label="Chloroquine"/>
-                <FormControlLabel value="MQ" control={<Radio/>} label="Mefloquine"/>
-                <FormControlLabel value="PPQ" control={<Radio/>} label="Piperaquine"/>
-                <FormControlLabel value="PYR" control={<Radio/>} label="Pyrimethamine"/>
-                <FormControlLabel value="SDX" control={<Radio/>} label="Sulfadoxine"/>
-              </RadioGroup>
-              <RadioGroup
-                aria-label="drug"
-                name="drug"
-                // className={classes.group}
-                value={drug}
-                onChange={this.handleChange}
-                style={{flexDirection: 'row'}}
-              >
-                <FormControlLabel value="SP" control={<Radio/>} label="Sulfadoxine-pyrimethamine"/>
-                <FormControlLabel value="SPIPTp" control={<Radio/>} label="Sulfadoxine-pyrimethamine for IPTp"/>
-                <FormControlLabel value="ASMQ" control={<Radio/>} label="Artesunate-mefloquine"/>
-                <FormControlLabel value="DHAPPQ" control={<Radio/>} label="Dihydro-artemisinin-piperaquine"/>
-              </RadioGroup>
-            </FormControl>
+              <FormControl component="fieldset">
+                <RadioGroup
+                  aria-label="drug"
+                  name="drug"
+                  // className={classes.group}
+                  value={drug}
+                  onChange={this.handleChange}
+                  style={{flexDirection: 'row'}}
+                >
+                  <FormControlLabel value="sites" control={<Radio/>} label="All drugs"/>
+                  <FormControlLabel value="ART" control={<Radio/>} label="Artemisinin"/>
+                  <FormControlLabel value="CQ" control={<Radio/>} label="Chloroquine"/>
+                  <FormControlLabel value="MQ" control={<Radio/>} label="Mefloquine"/>
+                  <FormControlLabel value="PPQ" control={<Radio/>} label="Piperaquine"/>
+                  <FormControlLabel value="PYR" control={<Radio/>} label="Pyrimethamine"/>
+                  <FormControlLabel value="SDX" control={<Radio/>} label="Sulfadoxine"/>
+                </RadioGroup>
+                <RadioGroup
+                  aria-label="drug"
+                  name="drug"
+                  // className={classes.group}
+                  value={drug}
+                  onChange={this.handleChange}
+                  style={{flexDirection: 'row'}}
+                >
+                  <FormControlLabel value="SP" control={<Radio/>} label="Sulfadoxine-pyrimethamine"/>
+                  <FormControlLabel value="SPIPTp" control={<Radio/>} label="Sulfadoxine-pyrimethamine for IPTp"/>
+                  <FormControlLabel value="ASMQ" control={<Radio/>} label="Artesunate-mefloquine"/>
+                  <FormControlLabel value="DHAPPQ" control={<Radio/>} label="Dihydro-artemisinin-piperaquine"/>
+                </RadioGroup>
+              </FormControl>
 
-            <div style={{width: '80vw', height: '60vh'}}>
-              <Map>
-                <TileLayer/>
-                {drug !== 'sites' ?
-                  <FeatureGroup>
-                      <TableGeoJSONsLayer
-                        onClickBehaviour="tooltip"
-                        onClickComponent="DocTemplate"
-                        onClickComponentProps={{drug_id:drug, dynamicSize: true, path: "templates/regionCloroplethTooltip.html"}}
-                        table="pf_regions"
-                        geoJsonProperty="geojson"
-                        max={100}
-                        min={0}
-                        showLegend={false}
-                        geoJsonStrokeOpacity={0.5}
-                        geoJsonFillOpacity={0}
-                      />
-                      <TableData
-                        table="pf_sites"
-                        area={["sql_max",[25,["*", [6, "num_samples"]]]]}
-                        name="name"
-                        lat="lat"
-                        lng="lng"
-                        colour={`${drug}resistance`}
-                        resistance={`${drug}resistance`}
-                        primKey="site_id"
-                        num_samples="num_samples"
-                      >
-                        <AttributeToColour table="pf_sites" property={`${drug}resistance`}>
-                          <MarkerLayer>
-                            <MarkerLayerMarker>
-                              <DocTemplate hideEditButton drug={drug} path="templates/circleMapMarker.html"/>
-                            </MarkerLayerMarker>
-                            <MarkerLayerPopup>
-                              <DocTemplate hideEditButton drug={drug} path="templates/drugResistanceInRegionSiteSamplesOnClickClusterTooltip.html"/>
-                            </MarkerLayerPopup>
-                          </MarkerLayer>
-                        </AttributeToColour>
-                      </TableData>
-                  </FeatureGroup>
-                  :
-                  <TableMarkersLayer
-                    clickPrimaryKeyProperty="site_id"
-                    table="pf_sites"
-                    clusterMarkers={false}
-                    onClickSingleBehaviour="tooltip"
-                    onClickSingleComponent="ItemTemplate"
-                    onClickSingleComponentTemplateDocPath="templates/siteOnClickTooltip.html"
-                    childDataColumns={['ARTresistance', 'CQresistance', 'MQresistance', 'PPQresistance', 'PYRresistance', 'SDXresistance']}
-                  >
-                    <SiteMarker flux={this.getFlux()}/>
-                  </TableMarkersLayer>
-                }
+              <div style={{width: '80vw', height: '60vh'}}>
+                <Map>
+                  <TileLayer/>
+                  {drug !== 'sites' ?
+                    <FeatureGroup>
+                        <TableGeoJSONsLayer
+                          onClickBehaviour="tooltip"
+                          onClickComponent="DocTemplate"
+                          onClickComponentProps={{drug_id:drug, dynamicSize: true, path: "templates/regionCloroplethTooltip.html"}}
+                          table="pf_regions"
+                          geoJsonProperty="geojson"
+                          max={100}
+                          min={0}
+                          showLegend={false}
+                          geoJsonStrokeOpacity={0.5}
+                          geoJsonFillOpacity={0}
+                        />
+                        <TableData
+                          table="pf_sites"
+                          area={["sql_max",[25,["*", [6, "num_samples"]]]]}
+                          name="name"
+                          lat="lat"
+                          lng="lng"
+                          colour={`${drug}resistance`}
+                          resistance={`${drug}resistance`}
+                          primKey="site_id"
+                          num_samples="num_samples"
+                        >
+                          <AttributeToColour table="pf_sites" property={`${drug}resistance`}>
+                            <MarkerLayer>
+                              <MarkerLayerMarker>
+                                <DocTemplate hideEditButton drug={drug} path="templates/circleMapMarker.html"/>
+                              </MarkerLayerMarker>
+                              <MarkerLayerPopup>
+                                <DocTemplate hideEditButton drug={drug} path="templates/drugResistanceInRegionSiteSamplesOnClickClusterTooltip.html"/>
+                              </MarkerLayerPopup>
+                            </MarkerLayer>
+                          </AttributeToColour>
+                        </TableData>
+                    </FeatureGroup>
+                    :
+                    <TableMarkersLayer
+                      clickPrimaryKeyProperty="site_id"
+                      table="pf_sites"
+                      clusterMarkers={false}
+                      onClickSingleBehaviour="tooltip"
+                      onClickSingleComponent="ItemTemplate"
+                      onClickSingleComponentTemplateDocPath="templates/siteOnClickTooltip.html"
+                      childDataColumns={['ARTresistance', 'CQresistance', 'MQresistance', 'PPQresistance', 'PYRresistance', 'SDXresistance']}
+                    >
+                      <SiteMarker flux={this.getFlux()}/>
+                    </TableMarkersLayer>
+                  }
 
-              </Map>
-            </div>
-            <div style={{display: 'flex', flexWrap:' wrap', paddingTop: '7px'}}>
-              <div style={{flexBasis: '128px'}}><DocTemplate path="templates/resistanceMapKey.html"/></div>
-              <div style={{flexBasis: '310px', flexGrow: '1', minWidth: '310px'}}><DocTemplate path="templates/resistanceMapLegend.html"/></div>
-            </div>
-          </CardContent>
-        </Card>
-      </div>);
+                </Map>
+              </div>
+              <div style={{display: 'flex', flexWrap:' wrap', paddingTop: '7px'}}>
+                <div style={{flexBasis: '128px'}}><DocTemplate path="templates/resistanceMapKey.html"/></div>
+                <div style={{flexBasis: '310px', flexGrow: '1', minWidth: '310px'}}><DocTemplate path="templates/resistanceMapLegend.html"/></div>
+              </div>
+            </CardContent>
+          </Card>
+        </div>
+        <div className="horiz-centering-container vertical stack obs-page-content">
+          <DocTemplate path="templates/footer.html"/>
+        </div>
+      </div>
+    );
   },
 });
 
